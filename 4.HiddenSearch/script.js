d24a0b17312ecc253d7a5c2142330366bd160f07
Gogulan12 <gogulanravi12@gmail.com>
2023-02-16 13:08:21 -0500

    Completed Day 4

---
+++ 4.HiddenSearch/script.js
@@ -0,0 +1,9 @@
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  // adds a line in the search bar to start typing
+  input.focus();
+});
