764eb4e659cf295165e624cd204a0427fc574ba2
Gogulan12 <gogulanravi12@gmail.com>
2023-02-22 11:51:25 -0500

    Completed Day 9

---
+++ 10.SoundBoard/script.js
@@ -0,0 +1,32 @@
+const sounds = [
+  "retro-game",
+  "laser",
+  "trailer",
+  "notification",
+  "game-over",
+  "futuristic-sound",
+];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
