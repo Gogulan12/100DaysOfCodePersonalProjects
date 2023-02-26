197220327ed013e7a5bd92f1c7f21de274cdaacd
Gogulan12 <gogulanravi12@gmail.com>
2023-02-26 11:10:29 -0500

    Completed Day 14

---
+++ 14.RandomChoicePicker/script.js
@@ -0,0 +1,74 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+// keyup - when you let go of key
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    // clears text area after enter
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  // console.log(tags);
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  // number of times its going to highlight each one before it stops
+  const times = 30;
+
+  // this shifts it to each tag - selecting and unselecting
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  // this function stops the random select and picks one
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  // when we use queryselectorall - its a node list we bring in - similar to an array with an index
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
